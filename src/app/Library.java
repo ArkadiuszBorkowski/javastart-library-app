d79a8ea57a97e6310b59f4c5e2845f3d00625fe7
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2022-08-22 20:17:08 +0200

    Add scanner to input data

--- src/app/Library.java
+++ src/app/Library.java
@@ -1,24 +1,24 @@
 package app;
+import io.DataReader;
 import model.Book;
+
+import java.util.Scanner;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.7";
+        final String appName = "Biblioteka v0.8";
 
         Book[] books = new Book[1000];
-        books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890");
-
-        books[1] = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009, 352, "Helion",
-                "9788324620845");
-        books[2] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
-                2008, 851, "McGraw-Hill Osborne Media");
+        DataReader dataReader = new DataReader();
 
         System.out.println(appName);
-        System.out.println("Książki dostępne w bibliotece:");
+        System.out.println("Wprowadź nową książkę:");
+        books[0] = dataReader.readAndCreateBook();
+        books[1] = dataReader.readAndCreateBook();
+        dataReader.close();
+
         books[0].printInfo();
         books[1].printInfo();
-        books[2].printInfo();
-
         System.out.println("System może przechowywać do " + books.length + " książek");
     }
 }
