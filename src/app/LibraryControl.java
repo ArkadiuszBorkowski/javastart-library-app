e0772bbdafe705e478dbce03e0ccab95670ceba3
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2022-10-17 14:47:56 +0200

    add private inner enum class - nested classes to LibraryControl class

--- src/app/LibraryControl.java
+++ src/app/LibraryControl.java
@@ -106,4 +106,33 @@ class LibraryControl {
         // zamykamy strumień wejścia
         dataReader.close();
     }
+
+    private enum Option {
+        EXIT(0, "Wyjście z programu"),
+        ADD_BOOK(1, "Dodanie książki"),
+        ADD_MAGAZINE(2,"Dodanie magazynu/gazety"),
+        PRINT_BOOKS(3, "Wyświetlenie dostępnych książek"),
+        PRINT_MAGAZINES(4, "Wyświetlenie dostępnych magazynów/gazet");
+
+        private int value;
+        private String description;
+
+        Option(int value, String desc) {
+            this.value = value;
+            this.description = desc;
+        }
+
+        @Override
+        public String toString() {
+            return value + " - " + description;
+        }
+
+        static Option createFromInt(int option) throws NoSuchOptionException {
+            try {
+                return Option.values()[option];
+            } catch(ArrayIndexOutOfBoundsException e) {
+                throw new NoSuchOptionException("Brak opcji o id " + option);
+            }
+        }
+    }
 }
