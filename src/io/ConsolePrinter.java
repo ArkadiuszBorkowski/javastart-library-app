ee962bb2cbdd7a92dbfe3277f84da14d7fa1e54e
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2022-12-29 17:30:00 +0100

    Dodanie strumienie do klasy Console Printer

--- src/io/ConsolePrinter.java
+++ src/io/ConsolePrinter.java
@@ -9,26 +9,23 @@ import java.util.Collection;
 
 public class ConsolePrinter {
     public void printBooks(Collection<Publication> publications) {
-        int counter = 0;
-        for (Publication publication : publications) {
-            if(publication instanceof Book) {
-                printLine(publication.toString());
-                counter++;
-            }
-        }
-        if (counter == 0)
+        long count = publications.stream()
+                .filter(publication -> publication instanceof Book)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if (count == 0) {
             printLine("Brak książek w bibliotece");
+        }
     }
 
     public void printMagazines(Collection<Publication> publications) {
-        int counter = 0;
-        for (Publication publication : publications) {
-            if(publication instanceof Magazine) {
-                printLine(publication.toString());
-                counter++;
-            }
-        }
-        if (counter == 0)
+        long count = publications.stream()
+                .filter(publication -> publication instanceof Magazine)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if (count == 0)
             printLine("Brak magazynów w bibliotece");
     }
 
@@ -37,10 +34,11 @@ public class ConsolePrinter {
         }
 
         public void printUsers(Collection<LibraryUser> users) {
-            for (LibraryUser user : users) {
-                printLine(user.toString());
-            }
+/*            for (LibraryUser user : users) {
+*//*                printLine(user.toString());
+            }*/
+            users.stream()
+                    .map(LibraryUser::toString)
+                    .forEach(this::printLine);
         }
-
-
 }
